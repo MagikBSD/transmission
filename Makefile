3cbd1f65fa35a82d697537f02f5e39daeaaeeba6
Grzegorz Blach <grzegorz@blach.pl>
2017-02-28 02:19:49 +0100

    Initial import

---
+++ Makefile
@@ -0,0 +1,31 @@
+BIN=transmission
+OBJ=${BIN}.o
+
+CC?=cc
+CFLAGS?=-O2 -march=native
+LDFLAGS?=
+LIBS?=-lucl
+PREFIX?=/usr/local
+
+
+all: ${BIN}
+
+${BIN}: ${OBJ}
+	${CC} ${LDFLAGS} -o $@ ${OBJ} ${LIBS}
+
+.c.o:
+	${CC} -c ${CFLAGS} -o $@ $<
+
+install: all
+	install -d ${DESTDIR}${PREFIX}/bin
+	install -m 0555 -s ${BIN} ${DESTDIR}${PREFIX}/bin
+
+test: ${BIN}
+	./${BIN} _test.conf _test.json
+	rm -f _test.json
+
+clean:
+	rm -f ${BIN} ${OBJ}
+
+
+${BIN}.o: ${BIN}.c
---
+++ transmission.c
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2017 Grzegorz Blach. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <error.h>
+#include <ucl.h>
+
+typedef struct cmdl {
+    ucl_emitter_t format;
+    char *in;
+    char *out;
+} cmdl_t;
+
+ucl_object_t *parse(char *fn) {
+    struct ucl_parser *parser = ucl_parser_new(0);
+    ucl_parser_add_file(parser, fn);
+    const char *error_msg = ucl_parser_get_error(parser);
+    if(error_msg) error(2, 0, "%s", error_msg);
+    ucl_object_t *conf = ucl_parser_get_object(parser);
+    ucl_parser_free(parser);
+    return conf;
+}
+
+void emit(char *fn, ucl_emitter_t format, ucl_object_t *conf) {
+    char *json = (char*) ucl_object_emit(conf, format);
+    FILE *file = fopen(fn, "w");
+    fputs(json, file);
+    fclose(file);
+}
+
+cmdl_t cmdl_parse(int ac, char **av) {
+    cmdl_t cmdl = { UCL_EMIT_JSON_COMPACT };
+
+    for(int i=1; i<ac; i++) {
+        if(av[i][0] == '-') {
+            switch(av[i][1]) {
+                case 'j': cmdl.format = UCL_EMIT_JSON; break;
+                case 'c': cmdl.format = UCL_EMIT_CONFIG; break;
+                case 'y': cmdl.format = UCL_EMIT_YAML; break;
+                case 'm': cmdl.format = UCL_EMIT_MSGPACK; break;
+            }
+        } else if(! cmdl.in) cmdl.in = av[i];
+        else if(! cmdl.out) cmdl.out = av[i];
+    }
+
+    return cmdl;
+}
+
+void help() {
+    puts(
+        "Transmission 0.1 by Grzegorz Blach\n" \
+        "Convert UCL config file to desired format.\n"
+        "\n" \
+        "Usage: transmission [-j|-c|-y|-m] <in file> <out file>\n" \
+        "\tEmit compacted JSON\n" \
+        " -j\tEmit fine formatted JSON\n" \
+        " -c\tEmit human readable config format\n" \
+        " -y\tEmit embedded YAML format\n" \
+        " -m\tEmit msgpack output"
+    );
+}
+
+int main(int ac, char **av) {
+    cmdl_t cmdl = cmdl_parse(ac, av);
+    if(! cmdl.out) {
+        help();
+        return 1;
+    }
+
+    ucl_object_t *conf = parse(cmdl.in);
+    emit(cmdl.out, cmdl.format, conf);
+    free(conf);
+
+    return 0;
+}
