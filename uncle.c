aa195cb80af320f9bac97406a881e882bb32a4c8
Grzegorz Blach <grzegorz@blach.pl>
2018-06-09 00:45:50 +0200

    Support for reading from stdin & writing to stdout

--- uncle.c
+++ uncle.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Grzegorz Blach. All rights reserved.
+ * Copyright (c) 2017-2018 Grzegorz Blach. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -24,6 +24,7 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <err.h>
 #include <ucl.h>
@@ -35,28 +36,45 @@ typedef struct cmdl {
 } cmdl_t;
 
 ucl_object_t *parse(char *fn) {
-    if(! access(fn, R_OK)) {
-        struct ucl_parser *parser = ucl_parser_new(0);
+    if(strcmp(fn, "-") && access(fn, R_OK)) err(2, "%s", fn);
+
+    struct ucl_parser *parser = ucl_parser_new(0);
+
+    if(strcmp(fn, "-")) {
         ucl_parser_add_file(parser, fn);
-        const char *error_msg = ucl_parser_get_error(parser);
-        if(error_msg) err(3, "%s", error_msg);
-        ucl_object_t *conf = ucl_parser_get_object(parser);
-        ucl_parser_free(parser);
-        return conf;
     } else {
-        err(2, "%s", fn);
+        int size = 0;
+        char chunk[1025];
+        char *data = malloc(0);
+        while(fgets(chunk, 1025, stdin) != NULL) {
+            size += 1024;
+            data = realloc(data, size+1);
+            strncat(data, chunk, 1024);
+        }
+        ucl_parser_add_string(parser, data, strlen(data));
+        free(data);
     }
+
+    const char *error_msg = ucl_parser_get_error(parser);
+    if(error_msg) err(3, "%s", error_msg);
+    ucl_object_t *conf = ucl_parser_get_object(parser);
+    ucl_parser_free(parser);
+    return conf;
 }
 
 void emit(char *fn, ucl_emitter_t format, ucl_object_t *conf) {
-    char *output = (char*) ucl_object_emit(conf, format);
-    FILE *file = fopen(fn, "w");
-    if(file) {
-        fputs(output, file);
-        fclose(file);
+    FILE *file;
+
+    if(fn) {
+        file = fopen(fn, "w");
+        if(! file) err(4, "%s", fn);
     } else {
-        err(4, "%s", fn);
+        file = stdout;
     }
+
+    char *output = (char*) ucl_object_emit(conf, format);
+    fputs(output, file);
+    if(fn) fclose(file);
 }
 
 cmdl_t cmdl_parse(int ac, char **av) {
@@ -69,6 +87,7 @@ cmdl_t cmdl_parse(int ac, char **av) {
                 case 'c': cmdl.format = UCL_EMIT_JSON_COMPACT; break;
                 case 'y': cmdl.format = UCL_EMIT_YAML; break;
                 case 'm': cmdl.format = UCL_EMIT_MSGPACK; break;
+                case '\0': cmdl.in = "-";
             }
         } else if(! cmdl.in) cmdl.in = av[i];
         else if(! cmdl.out) cmdl.out = av[i];
@@ -79,21 +98,24 @@ cmdl_t cmdl_parse(int ac, char **av) {
 
 void help(char *av0) {
     printf(
-        "UnCLe 0.1 by Grzegorz Blach\n" \
+        "UnCLe 0.2 by Grzegorz Blach\n" \
         "Convert UCL config file to desired format.\n"
         "\n" \
-        "Usage: %s [-j|-c|-y|-m] <in file> <out file>\n" \
+        "Usage: %s [-j|-c|-y|-m] <in file> [out file]\n" \
         "    \tEmit human readable config format\n" \
         "  -j\tEmit fine formatted JSON\n" \
         "  -c\tEmit compacted JSON\n" \
         "  -y\tEmit embedded YAML format\n" \
-        "  -m\tEmit msgpack output\n", av0
+        "  -m\tEmit msgpack output\n" \
+        "\n" \
+        "In file can be set to '-' for reading input from stdin.\n" \
+        "Out file can be ommited for writting output to stdout.\n", av0
     );
 }
 
 int main(int ac, char **av) {
     cmdl_t cmdl = cmdl_parse(ac, av);
-    if(! cmdl.out) {
+    if(! cmdl.in) {
         help(av[0]);
         return 1;
     }
